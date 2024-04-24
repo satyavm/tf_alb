051f9fb6b3e0c382f44bbc7fbb2c227ad1dbaff3
Satya Saranu <ssaranu@abc.com>
2024-04-23 20:52:16 -0500

    MODEFY: removed spaces ADD: adding code for ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,13 @@
 data "aws_ami" "amazon" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["al2023-ami-2023*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["137112412989"] # Canonical
 }
 
